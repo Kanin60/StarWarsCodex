b5c63aebffc3f2a3c101455a0f6d8a80a5721400
Amina <113165708+asmina18@users.noreply.github.com>
2023-10-03 10:26:46 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import { Film } from "./pages/Film/Film";
 import { Karakterer } from "./pages/Karakterer/Karakterer";
 import { Section1 } from "./pages/Section1/Section1";
 import { MainLayout } from "./layout/MainLayout";
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
+
 
 function App() {
-
+const queryClient= new QueryClient()
 
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainLayout />}>
           <Route index element={<Section1 />} />
           <Route path='/film' element={<Film />} />
           <Route path='/karakterer' element={<Karakterer />} />
+        
         </Route>
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/Film/Film.jsx
+++ src/pages/Film/Film.jsx
@@ -1,10 +1,52 @@
+import { useQuery } from "@tanstack/react-query"
+import { useState } from "react";
+import { request } from 'graphql-request'
+import { getAllFilms } from "../../queries/getAllFilm";
 
 export function Film() {
+    const [isModelOpen, setModelOpen] = useState(false)
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['getStarWarsFilms'],
+        queryFn: async () => request(`https://swapi-graphql.netlify.app/.netlify/functions/index`
+            , getAllFilms)
+    })
+    console.log(data);
+
+    const showModel = () => {
+        setModelOpen(true)
+    }
+
+    const closeModel = () => {
+        setModelOpen(false)
+    }
+
+    if (isLoading) return (
+        <div>
+            <p>Loading</p>
+        </div>
+    )
+    // if (error) return
+    //     <div>
+    //         Error:{error.massage}
+    //     </div>
 
     return (
         <section>
             <p>Her er FILMSIDEN!</p>
-
+            <div>
+                {data.allFilms.films.map((item, index) => {
+                    return (
+                        <p onClick={showModel} key={index}>
+                            {item.title}
+                        </p>
+                    )
+                })}
+                <Modal show={isModelOpen}>
+                    Message in Modal
+                    <button onClick={closeModel}>Close Model</button>
+                </Modal>
+            </div>
         </section>
+
     )
 }
